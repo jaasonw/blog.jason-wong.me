eb597eb4a06cb6ada062d1a516ed5dfd25e2bf6f
jaasonw <jayywong92@gmail.com>
2023-01-09 18:31:57 -0800

    html minifying + date formatting + responsive navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,40 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const mathjaxPlugin = require("eleventy-plugin-mathjax");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addPassthroughCopy("src/assets/css/markdown.css");
   eleventyConfig.addPassthroughCopy("src/assets/js/fontawesome.js");
   eleventyConfig.addPassthroughCopy("src/assets/css/prism.css");
   eleventyConfig.addPassthroughCopy("src/assets/css/prism-okaidia.css");
   eleventyConfig.addPassthroughCopy("src/assets/images/*");
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(mathjaxPlugin, {
-    output: "chtml",
+    output: "svg",
     chtml: {
       scale: 1.2,
       fontURL:
         "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",
     },
+    svg: {
+      scale: 1.2,
+    },
   });
-  // eleventyConfig.addCollection("posts", function (collection) {
-  //   return collection.getFilteredByGlob("/src/posts/*.md");
-  // });
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if (this.outputPath && this.outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        useShortDoctype: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
   return {
     dir: {
       input: "src",
