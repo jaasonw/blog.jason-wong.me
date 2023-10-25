21864654f8e1cc81ad1023541b11efd0cac346da
jaasonw <jayywong92@gmail.com>
2023-10-24 17:47:04 -0700

    disable code demo, upgrade to eleventy v2

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,13 @@ const mathjaxPlugin = require("eleventy-plugin-mathjax");
 const htmlmin = require("html-minifier");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const { EleventyPluginCodeDemo } = require("eleventy-plugin-code-demo");
+// const { EleventyPluginCodeDemo } = require("eleventy-plugin-code-demo");
 const timeToRead = require("eleventy-plugin-time-to-read");
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(UpgradeHelper);
   eleventyConfig.addPassthroughCopy("src/assets/js/fontawesome.js");
   eleventyConfig.addPassthroughCopy("src/assets/css/prism.css");
   eleventyConfig.addPassthroughCopy("src/assets/css/prism-okaidia.css");
@@ -23,25 +26,25 @@ module.exports = function (eleventyConfig) {
       scale: 1.2,
     },
   });
-  eleventyConfig.addPlugin(EleventyPluginCodeDemo, {
-    name: "codeDemo",
-    renderDocument: ({ html, css, js }) => `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <style>${css}</style>
-      </head>
-      <body>
-        ${html}
-        <script>${js}</script>
-      </body>
-    </html>`,
-    iframeAttributes: {
-      height: "100%",
-      style: "width: 100%;",
-      frameborder: "0",
-    },
-  });
+  // eleventyConfig.addPlugin(EleventyPluginCodeDemo, {
+  //   name: "codeDemo",
+  //   renderDocument: ({ html, css, js }) => `
+  //   <!DOCTYPE html>
+  //   <html>
+  //     <head>
+  //       <style>${css}</style>
+  //     </head>
+  //     <body>
+  //       ${html}
+  //       <script>${js}</script>
+  //     </body>
+  //   </html>`,
+  //   iframeAttributes: {
+  //     height: "100%",
+  //     style: "width: 100%;",
+  //     frameborder: "0",
+  //   },
+  // });
 
   eleventyConfig.addPlugin(timeToRead, {
     speed: "200 words a minute",
@@ -53,22 +56,22 @@ module.exports = function (eleventyConfig) {
       return data.timing;
     },
   });
-  eleventyConfig.addTransform("htmlmin", function (content) {
-    // Prior to Eleventy 2.0: use this.outputPath instead
-    if (this.outputPath && this.outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-        useShortDoctype: true,
-      });
-      return minified;
-    }
+  // eleventyConfig.addTransform("htmlmin", function (content) {
+  //   // Prior to Eleventy 2.0: use this.outputPath instead
+  //   if (this.outputPath && this.outputPath.endsWith(".html")) {
+  //     let minified = htmlmin.minify(content, {
+  //       useShortDoctype: true,
+  //       removeComments: true,
+  //       collapseWhitespace: true,
+  //       minifyCSS: true,
+  //       minifyJS: true,
+  //       useShortDoctype: true,
+  //     });
+  //     return minified;
+  //   }
 
-    return content;
-  });
+  //   return content;
+  // });
 
   // add anchors to blog headers
   let markdownLibrary = markdownIt({
